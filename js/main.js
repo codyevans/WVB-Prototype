8693fe0020659f5e377250ab3b36254115897fca
Cody Evans <codyevansdesign@gmail.com>
2013-10-08 10:10:46 -0600

    centered masonry working

--- js/main.js
+++ js/main.js
@@ -1,8 +1,67 @@
 
-// animations after load
-$(window).load(function() { // load the DOM then animate pages in
+// center isotope
+$.Isotope.prototype._getCenteredMasonryColumns = function() {
+this.width = this.element.width();
 
-	// run isotope
+var parentWidth = this.element.parent().width();
+
+              // i.e. options.masonry && options.masonry.columnWidth
+var colW = this.options.masonry && this.options.masonry.columnWidth ||
+              // or use the size of the first item
+              this.$filteredAtoms.outerWidth(true) ||
+              // if there's no items, use size of container
+              parentWidth;
+
+var cols = Math.floor( parentWidth / colW );
+cols = Math.max( cols, 1 );
+
+// i.e. this.masonry.cols = ....
+this.masonry.cols = cols;
+// i.e. this.masonry.columnWidth = ...
+this.masonry.columnWidth = colW;
+};
+
+$.Isotope.prototype._masonryReset = function() {
+// layout-specific props
+this.masonry = {};
+// FIXME shouldn't have to call this again
+this._getCenteredMasonryColumns();
+var i = this.masonry.cols;
+this.masonry.colYs = [];
+while (i--) {
+  this.masonry.colYs.push( 0 );
+}
+};
+
+$.Isotope.prototype._masonryResizeChanged = function() {
+var prevColCount = this.masonry.cols;
+// get updated colCount
+this._getCenteredMasonryColumns();
+return ( this.masonry.cols !== prevColCount );
+};
+
+$.Isotope.prototype._masonryGetContainerSize = function() {
+var unusedCols = 0,
+    i = this.masonry.cols;
+// count unused columns
+while ( --i ) {
+  if ( this.masonry.colYs[i] !== 0 ) {
+    break;
+  }
+  unusedCols++;
+}
+
+return {
+      height : Math.max.apply( Math, this.masonry.colYs ),
+      // fit container to columns that have been used;
+      width : (this.masonry.cols - unusedCols) * this.masonry.columnWidth
+    };
+};
+
+
+ $(function(){
+
+  	// // run isotope
 	function isotope() {
 
 		var $container = $('#artwork-container');
@@ -23,6 +82,12 @@ $(window).load(function() { // load the DOM then animate pages in
 	};
 
 	isotope();
+    
+ });  // end isotope
+
+
+// animations after load
+$(window).load(function() { // load the DOM then animate pages in
 
 	// preload this thing
 	$(".preloader").delay(350).fadeOut("slow");
