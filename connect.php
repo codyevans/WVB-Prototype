87768ef5f85f1dbbc255e36c8d6f5cb2d6409342
Cody Evans <codyevansdesign@gmail.com>
2013-10-08 21:51:16 -0600

    added connect page

---
+++ connect.php
@@ -0,0 +1,137 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>Wren Van Bockel | Connect</title>
+        <meta name="description" content="The Creative Portfolio of Wren Van Bockel">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="style.css">
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,800,700,300' rel='stylesheet' type='text/css'>
+        <script src="js/vendor/modernizr-2.6.2.min.js"></script>
+    </head>
+    <body id="about-page">
+
+        <div class="preloader">
+            <div class="centering-block">
+                <div class="centered-content">
+                   <svg class="svg" version="1.1" id="Layer_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+                     viewBox="0 0 62 90.81" enable-background="new 0 0 62 90.81" xml:space="preserve">
+                    <g>
+                        <g>
+                            <path class="pre-logo" d="M11.527,44.085c-0.027,0-0.051,0-0.078-0.003c-0.776-0.032-1.451-0.54-1.696-1.275C6.687,33.61,6.044,8.013,6.022,6.962
+                                h3.687c0.003,0.211,0.539,18.449,2.349,29.25l6.264-14.667c0.299-0.703,0.968-1.178,1.762-1.134
+                                c0.762,0.017,1.438,0.499,1.706,1.214l5.016,13.417c1.032-6.339,2.31-18.033,2.062-28.027h3.738
+                                c0.416,16.754-3.215,34.709-3.368,35.401c-0.18,0.809-0.871,1.399-1.696,1.455c-0.832,0.073-1.589-0.436-1.878-1.209
+                                l-5.739-15.353l-6.678,15.64C12.951,43.64,12.271,44.085,11.527,44.085z"/>
+                        </g>
+                        <path class="pre-logo" d="M53.369,33.45c-0.307-6.072-4.835-8.729-9.009-9.826c2.67-1.924,5.464-4.037,6.622-5.243
+                            c2.078-2.166,3.105-5.264,2.555-7.712c-0.379-1.679-1.184-2.991-2.761-3.59C50.567,7,49.985,6.846,49.764,6.845L35.121,6.911
+                            l-0.024,38.51h3.736V27.496c0.125-0.084,0.273-0.186,0.438-0.298c2.526-0.238,10.121-0.451,10.365,6.442
+                            c0.279,7.997,0.309,11.237,0.309,11.789l3.779,0.075C53.729,44.849,53.783,41.636,53.369,33.45z M49.42,10.749
+                            c0.199,0.126,0.38,0.336,0.47,0.741c0.274,1.215-0.416,3.065-1.604,4.301c-1.356,1.412-6.061,4.797-9.409,7.133
+                            c-0.015,0-0.029,0-0.044,0v-12.26L49.42,10.749z"/>
+                        <g>
+                            <path class="pre-logo" d="M11.422,82.71H7.684V49.38c0-0.859,0.584-1.606,1.417-1.813c0.828-0.212,1.699,0.178,2.103,0.935l15.982,30.056h1.161
+                                V49.433h3.738v30.992c0,1.032-0.835,1.869-1.869,1.869h-4.152c-0.691,0-1.326-0.379-1.65-0.99L11.422,56.875V82.71z"/>
+                        </g>
+                        <path class="pre-logo" d="M52.54,49.38H35.252v3.738h15.419v10.381h-5.71v3.738h5.71v12.876h-15.16v3.738H52.54c1.034,0,1.869-0.837,1.869-1.869
+                            V51.249C54.409,50.217,53.574,49.38,52.54,49.38z"/>
+                        <g>
+                            <path class="pre-logo" d="M60.12,90.81H1.869C0.836,90.81,0,89.973,0,88.94V1.927c0-1.032,0.836-1.869,1.869-1.869H60.12
+                                c1.032,0,1.869,0.837,1.869,1.869V88.94C61.989,89.973,61.152,90.81,60.12,90.81z M3.738,87.071h54.513V3.796H3.738V87.071z"/>
+                        </g>
+                    </g>
+                    </svg>
+                    <p>loading...</p>
+                </div>
+            </div>
+        </div>
+
+     <header class="gpu" id="header">
+            <a href="/" class="title">
+                Wren Van Bockel
+            </a>
+            <nav class="main-nav">
+                <ul>
+                    <li>
+                        <a href="artwork.php">artwork</a>
+                    </li>
+                    <li>
+                        <a href="about.php">about</a>
+                    </li>
+                    <li>
+                        <a href="connect.php">connect</a>
+                    </li>
+                </ul>
+            </nav>
+        </header>
+
+        <div clas="gpu" id="left"></div>
+        <div clas="gpu" id="right"></div>
+        <div clas="gpu" id="top"></div>
+        <div clas="gpu"id="bottom"></div>
+
+
+        <div class="page-background">
+
+           <div class="centering-block">
+                <div class="centered-content">
+                    <div id="intro-fade" class="gpu">
+                        <h1>Wren Van Bockel</h1>
+                        <div class="sub-heading">
+                           <p class="subtitle">
+                            <span>Say Hello</span>
+                           </p>
+                        </div>
+
+                        <div class="about-content">
+                            <p class="intro-content">
+                               Whimsical, bright, bold, and playful is the best way to describe Wren Van Bockel's artwork.
+                               Animals are often at the forefront of her work and are usually set in a colorful scenery.
+                               Her pieces read like cartoons or childhood story books. Often, young children are her biggest fans. 
+                               Creating art drives her quest for meaning and gives her the greatest joy.
+                            </p>
+                            <p>
+                                <strong>Andy Warhol</strong> once said, <blockquote>Don't think about making art, just get it done. 
+                                Let everyone else decide if it's good or bad, whether they love it or hate it. 
+                                While they are deciding, make even more art.</blockquote> 
+                            </p>
+                            <p>
+                               Wren was born in 1977 among the apple orchards of Wenatchee in Central Washington. 
+                               She began her schooling on Whidbey Island and culminated her formative years in Coeur d' Alene, Idaho. 
+                               During her childhood she cultivated her artistic talents, making dollhouse furniture, drawing fashion models, 
+                               and decorating the family tree house.  In 2001, Wren received a BFA from the University of Idaho. 
+                               Wren currently resides in Boise Idaho, where she continues to share and show her creations while expanding her portfolio.
+                            </p>
+                            <p>
+                                Wren's primary medium is acrylic on canvas and wood. She's recently moved into porcelain and mixed media to capture 
+                                her characters in a three-dimensional format.
+                            </p>
+                            <p>
+                                *BFA Painting and Drawing '01, University of Idaho
+                            </p>
+                        </div>
+
+                      
+                        <div class="copyright">
+                            <p>
+                                © <?php echo date('Y'); ?> Wren Van Bockel - All Rights Reserved
+                            </p>
+                        </div>
+                    </div>
+                </div>
+           </div>
+
+        </div> <!-- /page-background -->
+
+
+        <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.2.min.js"><\/script>')</script>
+        <script src="js/plugins.js"></script>
+        <script src="js/main.js"></script>
+    </body>
+</html>
